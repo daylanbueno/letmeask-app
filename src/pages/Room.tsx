72627697015e2d0d5aa360a7380b3ea70d30505a
DAILAN BUENO DOS SANTOS <x04501348143@tcu.gov.br>
2021-06-23 19:45:09 -0300

    criando sala de perguntas

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,5 +1,37 @@
+
+import logoImg from '../asserts/images/logo.svg'
+import { Button } from '../components/Button'
+
+import '../styles/room.scss'
+
 export function Room () {
     return (
-        <h2>Sala</h2>
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoImg} alt="LetmeasK" />
+                    <div>codigo</div>
+                </div>
+            </header>
+
+            <main className="main-content">
+                <div className="room-title">
+                    <h1>Sala React Q&A</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea
+                        placeholder="O que você quer perguntar?"
+                    />
+
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <button>faça seu login</button></span>
+                        <Button >Enviar pergunta</Button>
+                    </div>
+                </form>
+
+            </main>
+        </div>
     )
 }
